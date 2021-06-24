740ba0ce1dee6b9873abfc0a32a530afd52916c6
nxl6969 <nxl6969@g.rit.edu>
2021-06-25 00:24:29 +0200

    force new request with timestamp param on fetch endpoint

--- src/http/index.js
+++ src/http/index.js
@@ -1,8 +1,11 @@
 import axios from "axios";
+
 const COLOR_API_URL = 'https://www.colr.org/json/color/random'
 
 export function fetchRandomColor() {
-    return axios.get(COLOR_API_URL).then(response => {
+    return axios.get(COLOR_API_URL, {
+        params: {timestamp: (new Date()).getTime()}
+    }).then(response => {
         return response
     })
 }
